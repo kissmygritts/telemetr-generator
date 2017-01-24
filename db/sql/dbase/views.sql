b6405857671aac437e51f06d68e19c62c64c4080
kissmygritts <kissmygritts08@gmail.com>
2017-01-23 21:02:24 -0800

    additional views, procedures

--- db/sql/dbase/views.sql
+++ db/sql/dbase/views.sql
@@ -105,5 +105,21 @@ ORDER BY
   animals.perm_id,
   validity_codes.code;
 
--- TODO: analysis view
+CREATE VIEW relocs_analysis AS
+SELECT
+  animals.perm_id,
+  animals.species,
+  animals.sex,
+  devices.serial_num,
+  relocations.acq_time_lcl AS acq_time,
+  relocations.longitude,
+  relocations.latitude,
+  relocations.geom,
+  relocations.validity_code
+FROM (animals
+  INNER JOIN relocations ON animals.id = relocations.animal_id)
+  INNER JOIN devices ON relocations.device_id = devices.id
+WHERE relocations.validity_code IN (1, 3)
+ORDER BY animals.perm_id, relocations.acq_time_lcl;
+
 -- TODO: trajectory view
---
+++ db/sql/functions/improbable_relocations.sql
@@ -0,0 +1,63 @@
+CREATE OR REPLACE FUNCTION improbable_relocations()
+RETURNS bigint AS $$
+
+WITH rows AS (
+  UPDATE
+    relocations
+  SET
+    validity_code = 3
+  WHERE
+    id IN (
+      SELECT id
+      FROM
+        (SELECT
+          id,
+          ST_DistanceSpheroid(
+            geom,
+            LAG(geom, 1)
+            OVER (
+              PARTITION BY animal_id
+              ORDER BY acq_time_lcl),
+              'SPHEROID["WGS 84",6378137,298.257223563]') /
+            (EXTRACT(epoch FROM acq_time_lcl) -
+             EXTRACT(epoch FROM (
+                LAG(acq_time_lcl, 1)
+                OVER (
+                  PARTITION BY animal_id
+                  ORDER BY acq_time_lcl)))) * 3600 AS speed_from,
+          ST_DistanceSpheroid(
+            geom,
+            LEAD(geom, 1)
+            OVER (
+              PARTITION BY animal_id
+              ORDER BY acq_time_lcl),
+              'SPHEROID["WGS 84",6378137,298.257223563]') /
+            (- EXTRACT(epoch FROM acq_time_lcl) +
+             EXTRACT(epoch FROM (
+                LEAD(acq_time_lcl, 1)
+                OVER (
+                  PARTITION BY animal_id
+                  ORDER BY acq_time_lcl)))) * 3600 AS speed_to,
+          cos(
+            ST_Azimuth((LAG(geom, 1)
+              OVER (
+                PARTITION BY animal_id
+                ORDER BY acq_time_lcl))::geography,
+                geom::geography) -
+            ST_Azimuth(
+              geom::geography,
+              (LEAD(geom, 1)
+              OVER (
+                PARTITION BY animal_id
+                ORDER BY acq_time_lcl))::geography)) AS rel_angle
+        FROM relocations
+        WHERE validity_code = 1) t
+      WHERE
+        rel_angle < -.98 AND
+        speed_from > 300 AND
+        speed_to > 300)
+      RETURNING 1
+  )
+SELECT count(*) FROM rows;
+
+$$ LANGUAGE sql;
